74bce97acee4ecd46b354a44e5842b41848f396a
David Keci <dkeci21@epoka.edu.al>
2023-12-22 12:15:01 +0100

    Prova

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -459,101 +459,6 @@
         <div class="page-wrapper">
             <div class="content container-fluid">
 
-            <div class="page-header" style="margin-top: 0px;">
-                    <div class="row">
-                        <div class="col">
-                            <h3 class="page-title">Data Tables</h3>
-                            <div class="FilterContainer">
-                                <div class="reservation-box">
-                                    <div class="top">
-                                        <div class="static">
-                                            <label for="date-from" id="fromdate">From Date:</label>
-                                            <div class="input-container" id="date-picker-container">
-
-                                                <input type="date" id="date-checkin" class="date-field fromdateinput" name="date-from">
-                                            </div>
-                                        </div>
-                                        <div class="flex">
-                                            <label for="date-from" id="todate">To Date:</label>
-                                            <div class="input-container" id="date-picker-container">
-
-                                                <input type="date" id="date-checkout" class="date-field todateinput" name="">
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <button id="filterButton">Filter</button>
-                                <button id="clearButton">Clear</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /Page Header -->
-
-                <div class="row" style="margin-top: -30px;">
-                    <div class="col-sm-12 prova">
-                        <div class="card mb-0">
-
-                            <div class="card-body">
-
-                                <div class="table-responsive">
-
-                                    <table id="example" class="table datatable table table-stripped table-bordered mb-0">
-                                        <div class="searchTable">
-                                            <button id="excel">Exel</button>
-                                            <label for="search">Search</label>
-                                            <input type="text" id="searchTableButton">
-                                        </div>
-                                        <thead>
-                                            <tr>
-                                                <th>id</th>
-                                                <th>request</th>
-                                                <th>response</th>
-                                                <th>type</th>
-                                                <th>vatnumber</th>
-                                                <th>businessunit</th>
-                                                <th>tcr</th>
-                                                <th>aedid</th>
-                                                <th>createdat</th>
-                                                <th>status</th>
-                                                <th>lastretry</th>
-                                                <th>retrynr</th>
-                                                <th>extrafields</th>
-                                            </tr>
-                                        </thead>
-                                        </thead>
-                                        <tbody>
-
-
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th>id</th>
-                                                <th>request</th>
-                                                <th>response</th>
-                                                <th>type</th>
-                                                <th>vatnumber</th>
-                                                <th>businessunit</th>
-                                                <th>tcr</th>
-                                                <th>aedid</th>
-                                                <th>createdat</th>
-                                                <th>status</th>
-                                                <th>lastretry</th>
-                                                <th>retrynr</th>
-                                                <th>extrafields</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Page Header -->
-
             </div>
 
         </div>
@@ -568,18 +473,16 @@
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script> -->
 
 
+      <!-- Datatable JS -->
+      <script src="{{ asset('home/assets/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('home/assets/js/dataTables.bootstrap4.min.js') }}"></script>
 
     <!-- Bootstrap Core JS -->
     <script src="{{ asset('home/assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('home/assets/js/bootstrap.min.js') }}"></script>
 
-
     <!-- Slimscroll JS -->
     <script src="{{ asset('home/assets/js/jquery.slimscroll.min.js') }}"></script>
-
-
-
-
 
     <!-- Custom JS -->
     <script src="home/assets/js/app.js"></script>\
@@ -593,21 +496,18 @@
     <script src="{{ asset('home/assets/js/dataTables.bootstrap4.min.js') }}"></script>
 
 
-                <!-- tableFilter -->
+                <!-- tableFilter
                 <script type="module" src="{{ asset('js/Table/filterTable.min.js') }}"></script>
     <script type="module" src="{{ asset('js/Table/filterFunctionalities.min.js') }}"></script>
-    <script type="module" src="{{ asset('js/Table/search.min.js') }}"></script>
+    <script type="module" src="{{ asset('js/Table/search.min.js') }}"></script> -->
 
 
 
      <!-- ExportData -->
-    <script type="module" src="{{ asset('js/Table/Exportdata/exel.min.js') }}"></script>
+    <!-- <script type="module" src="{{ asset('js/Table/Exportdata/exel.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.0/xlsx.full.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script> -->
 
-      <!-- Datatable JS -->
-    <script src="{{ asset('home/assets/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('home/assets/js/dataTables.bootstrap4.min.js') }}"></script>
 
 </body>
 
